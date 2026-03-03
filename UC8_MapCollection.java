22cd187863bebdf693641b307d470f1477c32630
sv3895-commits <sv3895@srmist.edu.in>
2026-03-03 08:31:37 +0530

    Implemented UC7 - Map Collection

---
+++ UC8_MapCollection.java
@@ -0,0 +1,70 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class UC8_MapCollection {
+
+    // Method to build and return pattern map
+    private static Map<Character, String[]> buildPatternMap() {
+
+        Map<Character, String[]> patternMap = new HashMap<>();
+
+        patternMap.put('O', new String[]{
+                " *** ",
+                "*   *",
+                "*   *",
+                "*   *",
+                " *** "
+        });
+
+        patternMap.put('P', new String[]{
+                "**** ",
+                "*   *",
+                "**** ",
+                "*    ",
+                "*    "
+        });
+
+        patternMap.put('S', new String[]{
+                " ****",
+                "*    ",
+                " *** ",
+                "    *",
+                "**** "
+        });
+
+        return patternMap;
+    }
+
+    // Function to render banner
+    private static void renderBanner(String word, Map<Character, String[]> patternMap) {
+
+        int height = 5;
+
+        for (int row = 0; row < height; row++) {
+
+            StringBuilder line = new StringBuilder();
+
+            for (char ch : word.toCharArray()) {
+
+                String[] pattern = patternMap.get(ch);
+
+                if (pattern != null) {
+                    line.append(pattern[row]).append("  ");
+                } else {
+                    line.append("     ").append("  ");
+                }
+            }
+
+            System.out.println(line);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Map<Character, String[]> patternMap = buildPatternMap();
+
+        String word = "OOPS";
+
+        renderBanner(word, patternMap);
+    }
+}
